22ae377ced91a45c079cd5d2ecfc1b9fbdc55e5c
Suki <tues1205@naver.com>
2024-07-07 23:38:37 +0900

    SignUpActivity, firebase 연결

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
-    alias(libs.plugins.android.application)
+    //alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+
+    //firebase
+    id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -50,4 +54,9 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //firebase
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth-ktx")
 }
--- app/src/main/java/com/suki/chatting/SignUpActivity.kt
+++ app/src/main/java/com/suki/chatting/SignUpActivity.kt
@@ -1,20 +1,72 @@
 package com.suki.chatting
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.suki.chatting.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivitySignUpBinding
+    lateinit var mAuth: FirebaseAuth // 인증 서비스 객체
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+
+        // 인증 서비스 초기화
+        mAuth = Firebase.auth
+
+        // 가입하기 버튼 이벤트
+        binding.SignupBtn.setOnClickListener {
+
+            val email = binding.emailText.text.toString().trim()
+            val password = binding.passwordText.text.toString().trim()
+
+            signUp(email, password)
         }
+        
+    }
+
+    // 회원가입
+    private fun signUp(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) { // 회원가입 성공
+                    Toast.makeText(
+                        baseContext,
+                        "회원가입 성공!",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+
+                    val intent: Intent = Intent(this@SignUpActivity, MainActivity::class.java)
+                    startActivity(intent)
+
+                } else { // 회원가입 실패
+                    Toast.makeText(
+                        baseContext,
+                        "회원가입에 실패했습니다..",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    Log.d("SignUp Error", ": ${task.exception}")
+
+                }
+            }
     }
 }
